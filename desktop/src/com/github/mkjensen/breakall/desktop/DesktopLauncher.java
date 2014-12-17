6b4a5c82625d0ff6caa3d9c8ea5d76e576d5b239
Martin Kamp Jensen <martin.kamp.jensen@gmail.com>
2014-12-17 17:04:23 +0100

    Add files generated by gdx-setup.jar

---
+++ desktop/src/com/github/mkjensen/breakall/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.mkjensen.breakall.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.mkjensen.breakall.Breakall;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Breakall(), config);
+	}
+}
