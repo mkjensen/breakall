a3241f4e2fbe3d7fcc0e6ea35bf80d4b66a058f2
Martin Kamp Jensen <martin.kamp.jensen@gmail.com>
2014-12-20 16:51:00 +0100

    Box2D GWT

--- html/src/com/github/mkjensen/breakall/client/HtmlLauncher.java
+++ html/src/com/github/mkjensen/breakall/client/HtmlLauncher.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright 2014 Martin Kamp Jensen
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.github.mkjensen.breakall.client;
 
 import com.badlogic.gdx.ApplicationListener;
@@ -7,13 +20,13 @@ import com.github.mkjensen.breakall.Breakall;
 
 public class HtmlLauncher extends GwtApplication {
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
-        }
+  @Override
+  public GwtApplicationConfiguration getConfig() {
+    return new GwtApplicationConfiguration(800, 800);
+  }
 
-        @Override
-        public ApplicationListener getApplicationListener () {
-                return new Breakall();
-        }
+  @Override
+  public ApplicationListener getApplicationListener() {
+    return new Breakall();
+  }
 }
